37015dbd08952f7c8ebf6329fa37723774c7fcdc
dali <dalimiraoui48@gmail.com>
2025-02-12 16:11:23 +0000

    feat : add generic repo interface & its implement

---
+++ Product.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Product.Core.Interfaces;
+
+namespace Product.Infrastructure.Repository
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public Task AddAsync(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(params Expression<Func<T, bool>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, bool>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(T id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(T id, params Expression<Func<T, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T id, T Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
